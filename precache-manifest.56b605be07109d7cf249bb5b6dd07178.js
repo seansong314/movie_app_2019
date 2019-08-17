0f3f8b43d278895f7a4745f9d79ba097fc0e1924
Sean Song <seansong@berkeley.edu>
2019-08-16 21:09:42 -0700

    Updates

---
+++ precache-manifest.56b605be07109d7cf249bb5b6dd07178.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91c42885f433eb01e6b95014c6c4b40e",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "9b529e9c67121a4be770",
+    "url": "/movie_app_2019/static/css/main.e083b577.chunk.css"
+  },
+  {
+    "revision": "3cc7f05352d8eadd290c",
+    "url": "/movie_app_2019/static/js/2.6e87735a.chunk.js"
+  },
+  {
+    "revision": "9b529e9c67121a4be770",
+    "url": "/movie_app_2019/static/js/main.0827cc9b.chunk.js"
+  },
+  {
+    "revision": "5a2b9365f7ca01d151d6",
+    "url": "/movie_app_2019/static/js/runtime~main.64ac130e.js"
+  }
+]);
